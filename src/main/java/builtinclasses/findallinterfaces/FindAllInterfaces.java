d691c7ea3811642b3314dc76704a29eddf75e2e3
Calebe Dos Santos Oliveira <calebe.dso@gmail.com>
2025-03-04 14:38:20 -0300

    add findAllInterfaces example

---
+++ src/main/java/builtinclasses/findallinterfaces/FindAllInterfaces.java
@@ -0,0 +1,18 @@
+package builtinclasses.findallinterfaces;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class FindAllInterfaces {
+    public static Set<Class<?>> findAllImplementedInterfaces(Class<?> input) {
+        Set<Class<?>> allImplementedInterfaces = new HashSet<>();
+
+        Class<?>[] inputInterfaces = input.getInterfaces();
+        for (Class<?> currentInterface: inputInterfaces) {
+            allImplementedInterfaces.add(currentInterface);
+            allImplementedInterfaces.addAll(findAllImplementedInterfaces(currentInterface));
+        }
+
+        return allImplementedInterfaces;
+    }
+}
