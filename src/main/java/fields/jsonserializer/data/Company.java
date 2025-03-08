e3da5de37d0087ccf3626fbbe592ed82f22776ca
Calebe Dos Santos Oliveira <calebe.dso@gmail.com>
2025-03-08 17:40:07 -0300

    add json serializer example

---
+++ src/main/java/fields/jsonserializer/data/Company.java
@@ -0,0 +1,16 @@
+package fields.jsonserializer.data;
+
+public class Company {
+    String name;
+    String city;
+    Address address;
+
+    public Company(String name, String city, Address address) {
+        this.name = name;
+        this.city = city;
+        this.address = address;
+    }
+
+
+
+}
