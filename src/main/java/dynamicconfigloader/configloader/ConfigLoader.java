d99d22616c9bcd894be103560a287a913297325f
Calebe Dos Santos Oliveira <calebe.dso@gmail.com>
2025-03-10 09:32:38 -0300

    add array parsing in config loader example

--- src/main/java/dynamicconfigloader/configloader/ConfigLoader.java
+++ src/main/java/dynamicconfigloader/configloader/ConfigLoader.java
@@ -4,6 +4,7 @@ import dynamicconfigloader.data.GameConfig;
 import dynamicconfigloader.data.UserInterfaceConfig;
 
 import java.io.IOException;
+import java.lang.reflect.Array;
 import java.lang.reflect.Constructor;
 import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
@@ -47,12 +48,30 @@ public class ConfigLoader {
 
             field.setAccessible(true);
 
-            Object parsedValue = parseValue(field.getType(), propertyValue);
+            Object parsedValue;
+
+            if(field.getType().isArray()) {
+                parsedValue = parseArray(field.getType().getComponentType(), propertyValue);
+            } else {
+                parsedValue = parseValue(field.getType(), propertyValue);
+            }
+
 
             field.set(configInstance, parsedValue);
         }
 
         return configInstance;
+    }
+
+    private static Object parseArray(Class<?> arrayElementType, String value) {
+        String[] elementValues = value.split(",");
+
+        Object arrayObject = Array.newInstance(arrayElementType, elementValues.length);
+
+        for (int i = 0; i < elementValues.length; i++) {
+            Array.set(arrayObject, i, parseValue(arrayElementType, elementValues[i]));
+        }
+        return arrayObject;
     }
 
     private static Object parseValue(Class<?> type, String value) {
