dcbf551279d89ea8019263566e2b2ddbd9f02f99
Calebe Dos Santos Oliveira <calebe.dso@gmail.com>
2025-03-13 16:12:33 -0300

    add best game finder example

---
+++ src/main/java/annotations/bestgamesfinder/BestGamesFinder.java
@@ -0,0 +1,49 @@
+package annotations.bestgamesfinder;
+
+import static annotations.bestgamesfinder.annotationsbestgame.Annotations.*;
+import annotations.bestgamesfinder.databases.Database;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.SortedMap;
+import java.util.TreeMap;
+
+public class BestGamesFinder {
+    private Database database = new Database();
+
+    @Operation("All-Games")
+    public Set<String> getAllGames() {
+        return database.readAllGames();
+    }
+
+    @Operation("Game-To-Price")
+    public Map<String, Float> getGameToPrice(@DependsOn("All-Games") Set<String> allGames) {
+        return database.readGameToPrice(allGames);
+    }
+
+    @Operation("Game-To-Rating")
+    public Map<String, Float> getGameToRating(@DependsOn("All-Games") Set<String> allGames) {
+        return database.readGameToRatings(allGames);
+    }
+
+    @Operation("Score-To-Game")
+    public SortedMap<Double, String> scoreGames(@DependsOn("Game-To-Price") Map<String, Float> gameToPrice,
+                                                @DependsOn("Game-To-Rating") Map<String, Float> gameToRating) {
+        SortedMap<Double, String> gameToScore = new TreeMap<>(Collections.reverseOrder());
+
+        for(String gameName: gameToPrice.keySet()) {
+            double score = gameToRating.get(gameName) /  gameToPrice.get(gameName);
+            gameToScore.put(score, gameName);
+        }
+
+        return gameToScore;
+    }
+
+    @FinalResult
+    public List<String> getTopGames(@DependsOn("Score-To-Game") SortedMap<Double, String> gameToScore) {
+        return new ArrayList<>(gameToScore.values());
+    }
+}
